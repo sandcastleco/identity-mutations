02a537660f9cdf388dd5baac641956b54e6c70dd
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-01-30 16:22:54 -0600

    Update nav color, add background image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://unpkg.com/tachyons@4.9.1/css/tachyons.min.css"/>
     <link rel="stylesheet" href="/css/app.css">
   </head>
-  <body class="marvel bg-moon-gray">
+  <body style="background-image: url('{{ asset('images/background.jpg') }}');" class="marvel bg-moon-gray cover">
     <header class="vh-10 flex items-center ph3">
       <a class="db f1 link white" href="/">IM<span class="ims-black">(S)</span></a>
       {{-- <nav role="navigation">
@@ -27,25 +27,25 @@
 
     <div class="vh-80" role="main">
       <div class="grid">
-        <div class="panel bg-white-20 bb bw1 mr2">
+        <div class="panel bg-white-80 bb bw1 mr2">
           <div class="panel-header">
-            <a href="#">About</a>
+            <a class="ims-black f4" href="#">About</a>
           </div>
           <div class="panel-body">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eleifend, est quis molestie sodales, leo risus malesuada sem, eu tempus lacus orci ut velit. Mauris iaculis elit ac erat blandit, quis fringilla purus ornare. Pellentesque vitae interdum nunc. Vestibulum rhoncus enim non mollis consequat. Proin nunc tellus, finibus id tortor nec, varius malesuada massa. Ut placerat lacus id nisl egestas lobortis. In mattis at elit sed feugiat. In quis arcu rutrum velit viverra facilisis sed a orci. Mauris sed ex quis justo fringilla iaculis id eget elit. Etiam porta dolor a leo ultricies tempus. Cras faucibus ex pulvinar, feugiat est eu, vestibulum dui. Fusce at malesuada lectus. Suspendisse sit amet egestas ligula. Duis sit amet lacinia ligula, ut condimentum neque.</p>
           </div>
         </div>
-        <div class="panel bg-white-20 bb bw1 mr2">
+        <div class="panel bg-white-80 bb bw1 mr2">
           <div class="panel-header">
-            <a href="#">Contact</a>
+            <a class="ims-black f4" href="#">Contact</a>
           </div>
           <div class="panel-body">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eleifend, est quis molestie sodales, leo risus malesuada sem, eu tempus lacus orci ut velit. Mauris iaculis elit ac erat blandit, quis fringilla purus ornare. Pellentesque vitae interdum nunc. Vestibulum rhoncus enim non mollis consequat. Proin nunc tellus, finibus id tortor nec, varius malesuada massa. Ut placerat lacus id nisl egestas lobortis. In mattis at elit sed feugiat. In quis arcu rutrum velit viverra facilisis sed a orci. Mauris sed ex quis justo fringilla iaculis id eget elit. Etiam porta dolor a leo ultricies tempus. Cras faucibus ex pulvinar, feugiat est eu, vestibulum dui. Fusce at malesuada lectus. Suspendisse sit amet egestas ligula. Duis sit amet lacinia ligula, ut condimentum neque.</p>
           </div>
         </div>
-        <div class="panel bg-white-20 bb bw1 show">
+        <div class="panel bg-white-80 bb bw1 show">
           <div class="panel-header">
-            <a href="#">Gallery</a>
+            <a class="ims-black f4" href="#">Gallery</a>
           </div>
           <div class="panel-body w-100">
             @yield('content')
