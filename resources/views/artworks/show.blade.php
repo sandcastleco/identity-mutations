60e48926071dcf0bf85d888609383074cf72b757
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-01-30 17:06:12 -0600

    Fix inquiry form styling

--- resources/views/artworks/show.blade.php
+++ resources/views/artworks/show.blade.php
@@ -1,23 +1,30 @@
 @extends('page')
 
-@section('content')
+@section('main')
 
   <div class="w-100 h5 bg-gray"></div>
   <p>{{ $artwork->description }}</p>
   <p>{{ $artwork->price }} USD</p>
 
-  <form method="POST" action="/artwork/{{ $artwork->id }}/inquiry" class="mw6 center">
+  <form method="POST" action="/artwork/{{ $artwork->id }}/inquiry" class="mw6 center pb4">
 
     {{ csrf_field() }}
 
-    <label class="db mb1" for="name">Your name</label>
-    <input type="text" id="name" name="name" class="db w-100 pa2 marvel" required>
+    <div class="mb3">
+      <label class="db mb1" for="name">Your name</label>
+      <input type="text" id="name" name="name" class="db w-100 pa2 marvel ba b--ims-gray" required>
+    </div>
 
-    <label class="db mb1" for="email">Your e-mail address</label>
-    <input id="email" type="email" class="db w-100 pa2 marvel" name="email" value="{{ old('email') }}" required>
+    <div class="mb3">
+      <label class="db mb1" for="email">Your e-mail address</label>
+      <input id="email" type="email" class="db w-100 pa2 marvel ba b--ims-gray" name="email" value="{{ old('email') }}" required>
+    </div>
 
-    <textarea class="pa2 w-100 marvel" name="message">I would like to inquire about {{ $artwork->title }}</textarea>
-    <button type="submit">Send</button>
+    <div class="mb3">
+      <label class="db mb1" for="message">Your message</label>
+      <textarea class="pa2 w-100 marvel ba b--ims-gray" name="message">I would like to inquire about {{ $artwork->title }}.</textarea>
+    </div>
+    <button class="marvel pointer dib mt2 ph3 pv2 link ims-black ba b--ims-black bg-transparent ttu tracked" type="submit">Send</button>
   </form>
 
 @endsection
