e8b0fe412e04b4603b602fb79a3626a18eb70db2
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-02-02 17:47:21 -0600

    Disable registration

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Registration Routes...
 // Disabled
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
+// Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// Route::post('register', 'Auth\RegisterController@register');
 
 // Password Reset Routes...
 Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
