57356eb7166c98f0ded964bd521c6fa6eabf691a
Kevin McGillivray <kevin.mcgillivray@me.com>
2018-02-02 09:24:21 -0600

    Fix heading scroll bug

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,8 +1,8 @@
 <div class="panel mb3 mh3 ma0-ns flex bg-white-80 bl bl-0-ns bb-ns bw1 bw1-ns {{ $classes }}">
-  <div class="panel-header pointer w-100">
-    <h2 class="w-100 ims-black f4 mv0">{{ $title }}</h2>
+  <div class="panel-header pointer absolute w-100">
+    <h2 class="ims-black f4 mv0 w-100">{{ $title }}</h2>
   </div>
-  <div class="panel-body w-100">
+  <div class="panel-body w-100 ml4">
     @if(isset($wide))
       {{ $slot }}
     @else
